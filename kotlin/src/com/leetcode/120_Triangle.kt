9896cc9b6f247fac8b8c22b0cb1e01835d60a0b2
Mikhail Spitsin <spitsinmichael@gmail.com>
2020-03-30 17:28:16 +0400

    LeetCode 120 is improved

--- kotlin/src/com/leetcode/120_Triangle.kt
+++ kotlin/src/com/leetcode/120_Triangle.kt
@@ -2,8 +2,31 @@ package com.leetcode
 
 import java.lang.Integer.min
 
-//Recursion. Time: O(n), Space:  O(n^(1/2)), where n - is a total amount of numbers
+//DP, but we assume the we have natural numbers. Time: O(n), Space:  O(n), where n - is a total amount of numbers
 class Solution120 {
+    fun minimumTotal(triangle: List<List<Int>>): Int {
+        val buffer = IntArray(triangle.size)
+
+        triangle.forEachIndexed { i, row ->
+            if (i != 0) {
+                for (j in i downTo 0) {
+                    if (j != 0) {
+                        buffer[j] = if (j == i) buffer[j - 1] else min(buffer[j], buffer[j - 1])
+                    }
+                }
+            }
+
+            row.forEachIndexed { j, num ->
+                buffer[j] = buffer[j] + num
+            }
+        }
+
+        return buffer.min()!!
+    }
+}
+
+//Recursion. Will not good for big data. Time: O(2^n), Space:  O(n), where n - is a total amount of rows
+class Solution120old {
     fun minimumTotal(triangle: List<List<Int>>): Int = find(triangle, 0, 0, 0)
 
     private fun find(triangle: List<List<Int>>, i: Int, j: Int, sum: Int): Int {
