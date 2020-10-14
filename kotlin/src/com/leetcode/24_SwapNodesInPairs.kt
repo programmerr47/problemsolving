7fe6a85353a703c0d4437582288d8be9c5364bf3
Mikhail Spitsin <spitsinmichael@gmail.com>
2020-10-14 22:50:17 +0400

    Leetcode 24 is solved

---
+++ kotlin/src/com/leetcode/24_SwapNodesInPairs.kt
@@ -0,0 +1,72 @@
+package com.leetcode
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+private class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+private class Solution24 {
+    fun swapPairs(head: ListNode?): ListNode? {
+        val fakeHeader = ListNode(-1).also { it.next = head }
+        var current: ListNode? = fakeHeader
+        while (current != null) {
+            swapNext(current)
+            current = current.next?.next
+        }
+
+        return fakeHeader.next
+    }
+
+    private fun swapNext(node: ListNode) {
+        node.next?.let { next ->
+            val newNext = swap(next)
+            node.next = newNext
+        }
+    }
+
+    private fun swap(node: ListNode): ListNode {
+        val next = node.next ?: return node
+
+        val nextNext = next.next
+        next.next = node
+        node.next = nextNext
+
+        return next
+    }
+}
+
+fun main() {
+    println(Solution24().swapPairs(buildNotes(1, 2, 3, 4))?.toStrList())
+}
+
+private fun buildNotes(vararg values: Int): ListNode? {
+    if (values.isEmpty()) return null
+
+    val head = ListNode(values[0])
+    var current = head
+    for (i in 1..values.lastIndex) {
+        val next = ListNode(values[i])
+        current.next = next
+        current = next
+    }
+    return head
+}
+
+private fun ListNode.toStrList(): String {
+    var current: ListNode? = this
+    val list = ArrayList<Int>().apply {
+        while (current != null) {
+            add(current!!.`val`)
+            current = current!!.next
+        }
+    }
+    return list.toString()
+}
