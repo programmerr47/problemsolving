a1649da8a9ff01c68e610afd53e41624afcc4fd0
Mikhail Spitsin <spitsinmichael@gmail.com>
2020-11-17 13:51:34 +0400

    Leetcode 238 is solved

---
+++ kotlin/src/com/leetcode/238_ProductOfArrayExceptSelf.kt
@@ -0,0 +1,29 @@
+package com.leetcode
+
+//Time: O(n), Space: O(1)
+private class Solution238 {
+
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val result = IntArray(nums.size).sort()
+        var mult = 1
+
+        for (i in nums.indices) {
+            result[i] = mult
+            mult *= nums[i]
+        }
+
+        mult = 1
+
+        for (i in nums.lastIndex downTo 0) {
+            result[i] *= mult
+            mult *= nums[i]
+        }
+
+        return result
+    }
+
+}
+
+fun main() {
+    println(Solution238().productExceptSelf(intArrayOf(1, 2, 3, 4)).joinToString())
+}
